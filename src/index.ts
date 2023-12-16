3a8048f1097be440bdb045f5dcb255138b465ef4
Hiro <41129335+hiroshiuk1991@users.noreply.github.com>
2023-12-16 13:35:42 +0000

    feat: add dotenv dep

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import * as express from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello world, I'm running here");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
